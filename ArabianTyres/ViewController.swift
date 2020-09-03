9faa587c2dcd2b2dd3a53e27369015445f3f3aaf
Asif Khan <asif.khan@appinventiv.com>
2020-09-03 10:12:37 +0530

    # Initial Project set up Commit

---
+++ ArabianTyres/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArabianTyres
+//
+//  Created by Admin on 03/09/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
