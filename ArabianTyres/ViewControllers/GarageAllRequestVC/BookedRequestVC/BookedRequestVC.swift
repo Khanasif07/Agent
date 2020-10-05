40c82ec0c76e4fceb2a55ceed854b4c93873d69c
Asif Khan <asif.khan@appinventiv.com>
2020-10-05 16:31:39 +0530

    # worked on my garage service ui +  all request + booked request

---
+++ ArabianTyres/ViewControllers/GarageAllRequestVC/BookedRequestVC/BookedRequestVC.swift
@@ -0,0 +1,60 @@
+//
+//  BookedRequestVC.swift
+//  ArabianTyres
+//
+//  Created by Admin on 05/10/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+
+import UIKit
+
+class BookedRequestVC: BaseVC {
+    
+    // MARK: - IBOutlets
+    //===========================
+    @IBOutlet weak var mainTableView: UITableView!
+    // MARK: - Variables
+    //===========================
+    
+    // MARK: - Lifecycle
+    //===========================
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialSetup()
+    }
+    
+    // MARK: - IBActions
+    //===========================
+    
+    
+}
+
+// MARK: - Extension For Functions
+//===========================
+extension BookedRequestVC {
+    
+    private func initialSetup() {
+        self.mainTableView.delegate = self
+        self.mainTableView.dataSource = self
+        self.mainTableView.registerCell(with: BookedRequestTableCell.self)
+    }
+}
+
+// MARK: - Extension For TableView
+//===========================
+extension BookedRequestVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueCell(with: BookedRequestTableCell.self, indexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
---
+++ ArabianTyres/Xibs/PostloginVC/AllRequestedVC/BookedRequestTableCell.swift
@@ -0,0 +1,49 @@
+//
+//  ServiceRequestTableCell.swift
+//  ArabianTyres
+//
+//  Created by Admin on 05/10/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class BookedRequestTableCell: UITableViewCell {
+
+    @IBOutlet weak var serviceTimeLbl: UILabel!
+    @IBOutlet weak var serviceTyeLbl: UILabel!
+    @IBOutlet weak var ratingLbl: UILabel!
+    @IBOutlet weak var ratingImgView: UIImageView!
+    @IBOutlet weak var logoImgView: UIImageView!
+    @IBOutlet weak var chatBtn: AppButton!
+    @IBOutlet weak var startServiceBtn: AppButton!
+    @IBOutlet weak var ratingView: UIView!
+    @IBOutlet weak var dataContainerView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        initialSetUp()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        chatBtn.round(radius: 2.0)
+        startServiceBtn.round(radius: 2.0)
+        ratingImgView.round(radius: 4.0)
+        ratingView.round(radius: 2.0)
+        dataContainerView.addShadow(cornerRadius: 5, color: UIColor.black16, offset: CGSize(width: 0.5, height: 0.5), opacity: 1, shadowRadius: 5)
+    }
+    
+    
+    @IBAction func startServiceAction(_ sender: AppButton) {
+    }
+    
+    @IBAction func chatBtnAction(_ sender: AppButton) {}
+    
+    public func initialSetUp(){        self.logoImgView.backgroundColor = AppColors.fontTertiaryColor
+        chatBtn.isEnabled = true
+        startServiceBtn.isEnabled = true
+    }
+
+}
+
