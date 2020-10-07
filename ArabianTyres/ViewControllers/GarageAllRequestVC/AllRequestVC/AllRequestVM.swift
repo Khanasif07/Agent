9cbfc7ee0577273f348608a7671bcd0070dafada
arvind kumar saklani <arvind.saklani@appinventiv.com>
2020-10-07 18:29:37 +0530

    model Garage done.

--- ArabianTyres/ViewControllers/GarageAllRequestVC/AllRequestVC/AllRequestVM.swift
+++ ArabianTyres/ViewControllers/GarageAllRequestVC/AllRequestVC/AllRequestVM.swift
@@ -33,3 +33,32 @@ class AllRequestVM {
           }
       }
 }
+
+
+// MARK: - Result
+struct GarageRequestModel : Codable {
+    let id, createdAt: String
+    let preferredBrands: [PreferredBrand]
+    let preferredCountries: [String] = []
+    let status: String
+    let images: [String] = []
+    let width, profile, rimSize, quantity: Int
+    let requestType, requestID: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case createdAt, preferredBrands, preferredCountries, status, images, width, profile, rimSize, quantity, requestType
+        case requestID = "requestId"
+    }
+}
+
+// MARK: - PreferredBrand
+struct PreferredBrand: Codable {
+    let id, name: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case name
+    }
+}
+
