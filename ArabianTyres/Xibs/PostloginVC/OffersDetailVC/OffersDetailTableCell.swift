d7a3c16e8f3a43341ffbc13367d324deabc0bca7
arvind kumar saklani <arvind.saklani@appinventiv.com>
2020-10-23 10:02:07 +0530

    xib changes.

--- ArabianTyres/Xibs/PostloginVC/OffersDetailVC/OffersDetailTableCell.swift
+++ ArabianTyres/Xibs/PostloginVC/OffersDetailVC/OffersDetailTableCell.swift
@@ -19,9 +19,12 @@ class OffersDetailTableCell: UITableViewCell {
     @IBOutlet weak var unitLbl : UILabel!
     @IBOutlet weak var unitPrizeTextFiled : SkyFloatingLabelTextField!
     @IBOutlet weak var dashBackgroundView: UIView!
-    
+    @IBOutlet weak var totalPrizeLbl : UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        unitPrizeTextFiled.isHidden = true
+        unitPriceLbl.font = AppFonts.NunitoSansBold.withSize(13.0)
         unitPrizeTextFiled.selectedLineColor = .clear
         unitPrizeTextFiled.lineColor = .clear
         unitPrizeTextFiled.lineHeight = 0.0
@@ -32,8 +35,9 @@ class OffersDetailTableCell: UITableViewCell {
     func populateData(isBrandSelected: Bool,model: BidDatum){
         checkBtn.isSelected = isBrandSelected
         unitLbl.text = "\(model.quantity)"
-        unitPriceLbl.text = "\(model.amount)" + "SAR"
-        unitPrizeTextFiled.text = "\(Double(model.quantity) * model.amount)" + " SAR"
+        unitPriceLbl.text = "\(model.amount)" + " SAR"
+//        unitPrizeTextFiled.text = "\(Double(model.quantity) * model.amount)" + " SAR"
+        totalPrizeLbl.text = "\(Double(model.quantity) * model.amount)" + " SAR"
         brandNameLbl.text = "\(model.brandName)"
     }
     
