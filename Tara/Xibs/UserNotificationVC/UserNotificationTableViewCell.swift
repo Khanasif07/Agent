93873ac35d042555a049f7e6170ee7ce442e1210
arvind kumar saklani <arvind.saklani@appinventiv.com>
2020-11-02 13:25:56 +0530

    notification done.

--- Tara/Xibs/UserNotificationVC/UserNotificationTableViewCell.swift
+++ Tara/Xibs/UserNotificationVC/UserNotificationTableViewCell.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class UserNotificationTableViewCell: UITableViewCell {
 
+    //MARK:- IBOutlets
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var headingLbl: UILabel!
+    @IBOutlet weak var subHeadingLbl: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var timeLbl: UILabel!
+    @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var crossbtn: UIButton!
+
     //MARK:- Variables
     
     //MARK:-Life Cycle
@@ -17,5 +26,14 @@ class UserNotificationTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        containerView.createShadow(shadowColor: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1))
+        
+    }
+    
+    @IBAction func crossbtnAction(_ sender: UIButton) {
+        
+    }
 }
