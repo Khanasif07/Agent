3339e9cddf1fd85133b76d36c1a5b065c09e6c5f
Asif <asif.khan@appinventiv.com>
2021-03-20 12:56:20 +0530

    # Updated Code

--- Tara/Xibs/PaymentListingVC/PaymentListingCell.swift
+++ Tara/Xibs/PaymentListingVC/PaymentListingCell.swift
@@ -34,8 +34,8 @@ class PaymentListingCell: UITableViewCell {
     }
     
     func bindData(_ model: GarageRequestModel, screenType: ServiceCompletedVC.ScreenType) {
-        let type = model.requestType == .tyres ? LocalizedString.tyre.localized : model.requestType?.rawValue
-        serviceTypeLbl.text = (type ?? "") + LocalizedString.service.localized
+        let type = model.requestType?.rawValue == "Tyres" ? LocalizedString.tyre.localized : ( model.requestType?.rawValue == "Battery" ? LocalizedString.battery.localized : LocalizedString.oil.localized )
+        serviceTypeLbl.text = (type ) + LocalizedString.service.localized
         requestIdLbl.text = "#" + "\(model.requestID ?? "")"
         typeOfPaymentLbl.text = "\(model.paymentMode ?? "")"
         amountValueLbl.text =  "\(model.amount ?? 0.0)" + " " + LocalizedString.sar.localized
